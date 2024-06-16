11ef0196fadec8d88f6b3d9472b1e787b7857cc9
João Marcelo <159437965+jm-236@users.noreply.github.com>
2024-06-16 20:37:44 -0300

    Criação das classes Dev e Bootcamp.

---
+++ projeto/src/br/com/dio/desafio/dominio/Dev.java
@@ -0,0 +1,55 @@
+package br.com.dio.desafio.dominio;
+
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Dev {
+    private String nome;
+    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private Set<Conteudo> ConteudosConcluidos = new LinkedHashSet<>();
+
+    public void InscreverBootcamp(Bootcamp bootcamp){
+    }
+
+    public void progredir(){}
+
+    public void calcularTotalXp(){}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Set<Conteudo> getConteudosInscritos() {
+        return conteudosInscritos;
+    }
+
+    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
+        this.conteudosInscritos = conteudosInscritos;
+    }
+
+    public Set<Conteudo> getConteudosConcluidos() {
+        return ConteudosConcluidos;
+    }
+
+    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
+        ConteudosConcluidos = conteudosConcluidos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dev dev = (Dev) o;
+        return Objects.equals(getNome(), dev.getNome()) && Objects.equals(getConteudosInscritos(), dev.getConteudosInscritos()) && Objects.equals(getConteudosConcluidos(), dev.getConteudosConcluidos());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNome(), getConteudosInscritos(), getConteudosConcluidos());
+    }
+}
--- projeto/src/br/com/dio/desafio/dominio/Mentoria.java
+++ projeto/src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,26 +2,13 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
-    private String titulo;
-    private String descricao;
+public class Mentoria extends Conteudo{
+    @Override
+    public double calcularXp() {
+        return 0;
+    }
+
     private LocalDate date;
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public LocalDate getDate() {
         return date;
@@ -34,8 +21,8 @@ public class Mentoria {
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", date=" + date +
                 '}';
     }
